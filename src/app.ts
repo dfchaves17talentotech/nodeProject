7b98be8814ba77110b18f7461f8620ed1ac247bf
David Chaves <dfchaves17@gmail.com>
2024-08-16 08:01:05 -0500

    Add Controller for categories

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,24 @@
-import express from "express";
+import express, { Router } from "express";
 import pool from "./database/db_connect";
+import { getCategories } from "./controllers/categories_controller";
 
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT;
 
-app.get('/', async (req, res) => {
+const categoriesRoutes = Router();
+
+categoriesRoutes.get('/categories', getCategories);
+
+/* app.get('/', async (req, res) => {
     const query = 'select * from employees;';
     const response = await pool.query(query);
     console.log(response);
     res.send('Hola Mundo - Soy David Chaves');
-});
+}); */
+
+app.use(categoriesRoutes);
 
 app.listen(port, () =>{
     console.log(`Example app listening on port ${port}`)
